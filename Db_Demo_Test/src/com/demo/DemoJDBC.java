d609d6e310701fbb00f78f58bdf73cd908711609
chbhavanasri <139537330+chbhavanasri@users.noreply.github.com>
2024-09-24 12:14:03 +0530

    Update DemoJDBC.java

--- Db_Demo_Test/src/com/demo/DemoJDBC.java
+++ Db_Demo_Test/src/com/demo/DemoJDBC.java
@@ -2,7 +2,7 @@ class JDBCDemoMain{
 	
 	public static void main(String args){
 		
-		System.out.println("Hello Codebegun::: GITGUB");
+		System.out.println("Hello Codebegun::: lllll GITGUB");
 		
 		System.out.println("Hello Codebegun::: GITGUB"+"  SURYA");
 		
